61d17e2fcd706e0b625230530ad9404ac2a98e1f
Evi Kadar <evi.kadar@gmail.com>
2018-10-25 11:38:29 +0200

    New clock hand can be created with chosen color

--- static/js/rotate.js
+++ static/js/rotate.js
@@ -1,21 +1,13 @@
-// Argument color should be added, getting it from the form the user provided.
-function changeColor(color) {
-    $('#colorRed').on('click', function () {
-        $('#path1').css({fill: color})
-    });
-}
 
 // The animate to function should have a parameter that tells it where it should point.
 function rotateAround(degree, initialDegree) {
     $('#rotateBtn').click(function () {
-        console.log('Rotate clicked');
-        $('#clockhand').rotate(
+        $('#smallHandle').rotate(
             {
                 animateTo: degree,
                 center: ["50%", "100%"],
                 callback: function () {
                     let degreeChange = degree % 360 - initialDegree;
-                    console.log(degreeChange);
                 }
             })
     });
@@ -23,15 +15,25 @@ function rotateAround(degree, initialDegree) {
 
 function createNewHand(color, degree) {
     $('#newHandBtn').click(function () {
-        let newHand = $('.clockHandDiv').clone();
-        console.log(newHand);
+        let newHand = $('#smallHandle').clone();
+        newHand.children().css({fill: color});
+        newHand.removeClass('floor1');
+        if ($('.floor3').length < 1) {
+            newHand.addClass('floor3');
+        } else if ($('.floor4').length < 1) {
+            newHand.addClass('floor4');
+        } else if ($('.floor5').length < 1) {
+            newHand.addClass('floor5');
+        } else {
+            alert('You cant add more than 4 persons in this MVP version');
+        }
+        $('.handCloner').append(newHand);
     });
 }
 
 function main() {
-    changeColor('#ff43aa');
     rotateAround(20, 0);
-    createNewHand();
+    createNewHand('#00da10');
 }
 
 main();
