6c0933f5c77954dd6ade3c5403693bf8defc8b06
Evi Kadar <evi.kadar@gmail.com>
2018-10-25 14:48:58 +0200

    Demo buttons uploaded for showing new clockhand creation and coloring

--- static/js/rotate.js
+++ static/js/rotate.js
@@ -1,4 +1,3 @@
-
 // The animate to function should have a parameter that tells it where it should point.
 function rotateAround(degree, initialDegree) {
     $('#rotateBtn').click(function () {
@@ -14,26 +13,29 @@ function rotateAround(degree, initialDegree) {
 }
 
 function createNewHand(color, degree) {
-    $('#newHandBtn').click(function () {
-        let newHand = $('#smallHandle').clone();
-        newHand.children().css({fill: color});
-        newHand.removeClass('floor1');
-        if ($('.floor3').length < 1) {
-            newHand.addClass('floor3');
-        } else if ($('.floor4').length < 1) {
-            newHand.addClass('floor4');
-        } else if ($('.floor5').length < 1) {
-            newHand.addClass('floor5');
-        } else {
-            alert('You cant add more than 4 persons in this MVP version');
-        }
-        $('.handCloner').append(newHand);
-    });
+    let newHand = $('#smallHandle').clone();
+    newHand.children().css({fill: color});
+    newHand.removeClass('floor1');
+    if ($('.floor3').length < 1) {
+        newHand.addClass('floor3');
+    } else if ($('.floor4').length < 1) {
+        newHand.addClass('floor4');
+    } else if ($('.floor5').length < 1) {
+        newHand.addClass('floor5');
+    } else {
+        alert('You cant add more than 4 persons in this MVP version');
+    }
+    $('.handCloner').append(newHand);
 }
 
 function main() {
-    rotateAround(20, 0);
-    createNewHand('#00da10');
+    rotateAround(240, 0);
+    $('#newHandBtn').click(function () {
+        createNewHand('#00da10')
+    })
+    $('#newHandBtnPurple').click(function () {
+        createNewHand('#9400D3')
+    })
 }
 
 main();
